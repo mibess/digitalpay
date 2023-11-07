014e5436d4baae1cd64cadd456116a4396799665
mibess <mibexzooo@gmail.com>
2023-11-07 17:51:39 -0300

    first commit

---
+++ src/main/java/com/mibess/digitalpay/DigitalpayApplication.java
@@ -0,0 +1,13 @@
+package com.mibess.digitalpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalpayApplication.class, args);
+	}
+
+}
